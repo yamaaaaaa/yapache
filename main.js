402fb3ec231bfbf616b0e0e9e3f3047abdf76f0e
Takamori Yamashiro <>
2019-03-17 06:12:40 +0900

    change url to ip

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ const { app,ipcMain,BrowserWindow } = require("electron");
 var ipc = require('ipc');
 let win;
 
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var ipAddress;
+ 
+Object.keys(ifaces).forEach(function (ifname) {
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+    console.log(ifname, iface.address);
+    // en0 192.168.1.NNN
+    ipAddress = iface.address;
+  });
+});
+
+console.log(ipAddress);
 
 function createWindow(){
 	win = new BrowserWindow({width: 300,height: 300});
@@ -9,14 +26,14 @@ function createWindow(){
 	win.on("closed",() => {win = null; });
 }
 
-function startServer(documentRoot){		
-
+function startServer(documentRoot){
 	var express = require('express');
 	var express_app = express();
 	express_app.use(express.static(documentRoot));
-	// express_app.use(express.static('/Users/taka/_github/_sandbox/electron/02-server/yapache/yapache-darwin-x64'));
 	express_app.listen(8888, ()=> {
 	  console.log('Express Server 01');
+	  const {shell} = require('electron');
+	  shell.openExternal('http://'+ipAddress+':8888/');
 	});
 }
 
