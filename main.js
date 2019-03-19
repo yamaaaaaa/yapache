f23548fce0ba697962bffb19c49ec8066ad35fde
Takamori Yamashiro <>
2019-03-19 16:46:05 +0900

    add QR-code

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const { app,ipcMain,BrowserWindow } = require("electron");
-var ipc = require('ipc');
 let win;
-
 var os = require('os');
 var ifaces = os.networkInterfaces();
 var ipAddress;
@@ -9,35 +7,17 @@ var ipAddress;
 Object.keys(ifaces).forEach(function (ifname) {
   ifaces[ifname].forEach(function (iface) {
     if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
     }
-    console.log(ifname, iface.address);
-    // en0 192.168.1.NNN
     ipAddress = iface.address;
   });
 });
 
-console.log(ipAddress);
-
-function createWindow(){
-	win = new BrowserWindow({width: 300,height: 300});
+app.on('ready',()=>{
+	win = new BrowserWindow({width: 400,height: 600});
 	win.loadURL('file://'+__dirname+'/index.html');
 	win.on("closed",() => {win = null; });
-}
-
-function startServer(documentRoot){
-	var express = require('express');
-	var express_app = express();
-	express_app.use(express.static(documentRoot));
-	express_app.listen(8888, ()=> {
-	  console.log('Express Server 01');
-	  const {shell} = require('electron');
-	  shell.openExternal('http://'+ipAddress+':8888/');
-	});
-}
-
-app.on('ready',createWindow);
+});
 app.on('window-all-closed',()=>{
 	if(process.platform !== 'darwin'){
 		app.quit();
@@ -50,9 +30,17 @@ app.on('activate',()=>{
 });
 
 ipcMain.on('select', function( event, documentRoot ){
-	startServer(documentRoot);
+	var url = 'http://'+ipAddress+':8888/';
+	var express = require('express');
+	var express_app = express();
+	express_app.use(express.static(documentRoot));
+	express_app.listen(8888, ()=> {
+		const {shell} = require('electron');
+	  	shell.openExternal(url);
+	});
+	event.sender.send('init-qr', url);
 });
+
 ipcMain.on('stop', function( event , arg){
 	app.quit();
 });
-
